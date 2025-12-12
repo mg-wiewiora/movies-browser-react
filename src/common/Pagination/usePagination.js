4fd755db16b1eb5a0a552507fe04dc5036d6da27
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-12-12 11:32:17 +0100

    formatting improved in usePagination

--- src/common/Pagination/usePagination.js
+++ src/common/Pagination/usePagination.js
@@ -5,8 +5,8 @@ export const usePagination = (totalItems, itemsPerPage) => {
   const totalPages = Math.ceil(totalItems / itemsPerPage);
 
   const goToFirst = () => setPage(1);
-  const goToPrev = () => setPage(prev => Math.max(prev - 1, 1));
-  const goToNext = () => setPage(prev => Math.min(prev + 1, totalPages));
+  const goToPrev = () => setPage((prev) => Math.max(prev - 1, 1));
+  const goToNext = () => setPage((prev) => Math.min(prev + 1, totalPages));
   const goToLast = () => setPage(totalPages);
 
   return { page, totalPages, goToFirst, goToPrev, goToNext, goToLast, setPage };
