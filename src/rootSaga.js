7134d9cc67bdf2e660fb987851dea5eed41707bc
Izabela Janusz <afrodyzjak100@gmail.com>
2025-12-12 17:30:35 +0100

    adding to rootSaga peopleSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,6 +1,8 @@
 import { all } from "redux-saga/effects";
 import { moviesSaga } from "./features/movies/moviesSaga";
+import { peopleSaga } from "./features/people/peopleSaga";
 
 export default function* rootSaga() {
   yield all([moviesSaga()]);
+  yield all([peopleSaga()]);
 }
