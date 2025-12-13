429379f6cd871271c6bc2d40a9c2d127b70f51f1
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-12-13 10:08:19 +0100

    api.js added and configured for better arrangement

--- src/features/movies/MoviesPage/index.js
+++ src/features/movies/MoviesPage/index.js
@@ -3,20 +3,34 @@ import Section from "../../../common/Section";
 import Tile from "./Tile";
 import { MoviesGrid } from "./styled";
 import { Pagination } from "../../../common/Pagination";
-import { useMoviesPage } from "./useMoviesPage";
+import { usePagination } from "../../../common/Pagination/usePagination";
+import {
+  ITEMS_PER_PAGE,
+  TOTAL_PAGES,
+  TMDB_ITEMS_PER_PAGE,
+} from "../moviesConstants";
+import { getPageData } from "./moviesData";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMoviesStart } from "../moviesSlice";
 
 const MoviesPage = () => {
-  const {
-    moviesToShow,
-    loading,
-    error,
+  const dispatch = useDispatch();
+  const { movies, loading, error } = useSelector((state) => state.movies);
+
+  const { page, totalPages, goToFirst, goToPrev, goToNext, goToLast } =
+    usePagination(TOTAL_PAGES, ITEMS_PER_PAGE, TOTAL_PAGES);
+
+  const { tmdbPage, startIndex, endIndex } = getPageData(
     page,
-    totalPages,
-    goToFirst,
-    goToPrev,
-    goToNext,
-    goToLast,
-  } = useMoviesPage();
+    ITEMS_PER_PAGE,
+    TMDB_ITEMS_PER_PAGE
+  );
+  const moviesToShow = movies.slice(startIndex, endIndex);
+
+  useEffect(() => {
+    dispatch(fetchMoviesStart({ page: tmdbPage }));
+  }, [dispatch, tmdbPage]);
 
   return (
     <Container>
--- src/features/movies/moviesSaga.js
+++ src/features/movies/moviesSaga.js
@@ -1,36 +1,23 @@
 import { call, put, takeLatest, all } from "redux-saga/effects";
-import axios from "axios";
+import { tmdbApi } from "./api";
 import {
   fetchMoviesStart,
   fetchMoviesSuccess,
   fetchMoviesFailure,
 } from "./moviesSlice";
 
-const API_KEY = "e0da2a33c4def495d0c4977083b2de8b";
-const BASE_URL = "https://api.themoviedb.org/3";
-
 function fetchPopularMoviesApi(page) {
-  return axios.get(`${BASE_URL}/movie/popular`, {
-    params: {
-      api_key: API_KEY,
-      language: "en-US",
-      page,
-    },
-  });
+  return tmdbApi.get("/movie/popular", { params: { language: "en-US", page } });
 }
 
 function fetchGenresApi() {
-  return axios.get(`${BASE_URL}/genre/movie/list`, {
-    params: {
-      api_key: API_KEY,
-      language: "en-US",
-    },
-  });
+  return tmdbApi.get("/genre/movie/list", { params: { language: "en-US" } });
 }
 
 function* fetchMoviesSaga(action) {
   try {
-    const page = action.payload.page;
+    const page = action.payload?.page || 1;
+
     const [moviesResponse, genresResponse] = yield all([
       call(fetchPopularMoviesApi, page),
       call(fetchGenresApi),
