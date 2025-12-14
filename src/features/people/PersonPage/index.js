df8ad0f8bffefbcd44ed22227e388302f7b0677e
Izabela Janusz <afrodyzjak100@gmail.com>
2025-12-14 19:22:25 +0100

    adding to PersonPage title and adding to images people.jpg

--- src/features/people/PersonPage/index.js
+++ src/features/people/PersonPage/index.js
@@ -1,10 +1,13 @@
 import Section from "../../../common/Section";
 import { Container } from "../../../common/Container/styled";
+import Tile from "../../../common/Tile";
+import poster from "../../../images/people.jpg";
 
 const PersonPage = () => {
   return (
     <Container>
       <Section title="Title" content="Content" />
+      <Tile image={poster} title="Scarlett Johansson" content="Content" />
     </Container>
   );
 };
