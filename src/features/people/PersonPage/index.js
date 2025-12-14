89beee7afa0a52b81d20a40dafcf52968a6bef70
Izabela Janusz <afrodyzjak100@gmail.com>
2025-12-15 00:15:00 +0100

    changes in PersonPage

--- src/features/people/PersonPage/index.js
+++ src/features/people/PersonPage/index.js
@@ -1,13 +1,16 @@
 import Section from "../../../common/Section";
 import { Container } from "../../../common/Container/styled";
-import Tile from "../../../common/Tile";
+
 import poster from "../../../images/people.jpg";
+import Tiles from "../../../common/Tiles";
+
+
 
 const PersonPage = () => {
   return (
     <Container>
       <Section title="Title" content="Content" />
-      <Tile image={poster} title="Scarlett Johansson" content="Content" />
+      <Tiles people={poster} title="Scarlett Johansson" content="Content" />
     </Container>
   );
 };
