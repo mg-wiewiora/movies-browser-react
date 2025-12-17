93de713d1dfc1622fe744bd8ebd6476764ee7280
Michał Kowalczyk <michalkowal3k@gmail.com>
2025-12-17 06:27:08 +0100

    PeoplePage using Tiles from different pages

--- src/features/people/PersonPage/index.js
+++ src/features/people/PersonPage/index.js
@@ -1,10 +1,15 @@
 import Section from "../../../common/Section";
 import { Container } from "../../../common/Container/styled";
+import TileMovie from "../../movies/MoviePage/TileMovie/index.js";
+import Tile from "../../movies/MoviesPage/Tile/index.js";
 
 const PersonPage = () => {
+
   return (
     <Container>
-      <Section title="Title" content="Content" />
+      <Section content={<TileMovie movie={[]}/>} />
+      <Section title="Movies Cast" content={<Tile movie={[]}/>} />
+      <Section title="Movies Crew" content={<Tile movie={[]}/>} />
     </Container>
   );
 };
