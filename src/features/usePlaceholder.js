ed7a95ec04d184153cd84463a959201988fc4f45
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-12-18 07:02:28 +0100

    usePlaceholder hook improved

--- src/features/usePlaceholder.js
+++ src/features/usePlaceholder.js
@@ -1,11 +1,8 @@
 import { useLocation } from "react-router-dom";
-import { toPeople } from "../routes";
 
 export const usePlaceholder = () => {
   const { hash } = useLocation();
-  const path = hash.slice(1);
-
-  return path === toPeople() || path.startsWith("/person")
+  return hash.includes("/people") || hash.includes("/person")
     ? "Search for people..."
     : "Search for movies...";
 };
