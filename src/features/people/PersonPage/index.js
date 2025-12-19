1eabe1e773cf02427f89ce7f5df01812e977acd8
Michał Kowalczyk <michalkowal3k@gmail.com>
2025-12-19 19:50:27 +0100

    Trying to show proper movies in PersonPage

--- src/features/people/PersonPage/index.js
+++ src/features/people/PersonPage/index.js
@@ -2,14 +2,18 @@ import Section from "../../../common/Section";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
+import { MoviesGrid } from "./styled";
 import { Container } from "../../../common/Container/styled";
 import { fetchPersonStart } from "../personSlice";
+import { fetchMovieStart } from "../../movies/movieSlice";
 import TileDetails from "../../../common/TileDetails/index.js";
 import Tile from "../../movies/MoviesPage/Tile/index.js";
 
 const PersonPage = () => {
 
   const { person, personLoading, personError } = useSelector((state) => state.person);
+  const { movies, loading, error } = useSelector((state) => state.movies);
+  const { credits, creditsLoading, creditsError } = useSelector((state) => state.credits);
   const { id } = useParams();
   const dispatch = useDispatch();
 
@@ -18,6 +22,17 @@ const PersonPage = () => {
       dispatch(fetchPersonStart(id));
     }
   }, [dispatch, id]);
+
+  useEffect(() => {
+    if (id) {
+      dispatch(fetchMovieStart(id));
+    }
+  }, [dispatch, id]);
+
+  console.log({ movies })
+  console.log({ credits })
+  const castList = credits?.cast || [];
+  const crewList = credits?.crew || [];
 
   return (
     <Container>
@@ -30,7 +45,12 @@ const PersonPage = () => {
             show={false}
             person={person} />
         } />
-      <Section title="Movies Cast" content={<Tile movie={person} />} />
+      <Section
+        title="Movies Cast"
+        content={
+        <>
+          </>
+         } />
       <Section title="Movies Crew" content={<Tile movie={[]} />} />
     </Container>
   );
