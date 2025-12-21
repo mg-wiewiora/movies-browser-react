d31a1ec83b110ee29bc5be33a7334ab4ac0e1fea
Malgorzata Wiewiora <mg.wiewiora@gmail.com>
2025-12-21 17:05:59 +0100

    Tile import changed to MoviesTile in PersonPage

--- src/features/people/PersonPage/index.js
+++ src/features/people/PersonPage/index.js
@@ -1,15 +1,15 @@
 import Section from "../../../common/Section";
 import { Container } from "../../../common/Container/styled";
 import TileMovie from "../../movies/MoviePage/TileMovie/index.js";
-import Tile from "../../movies/MoviesPage/Tile/index.js";
+import MoviesTile from "../../movies/MoviesPage/MoviesTile/index.js";
 
 const PersonPage = () => {
 
   return (
     <Container>
       <Section content={<TileMovie movie={[]}/>} />
-      <Section title="Movies Cast" content={<Tile movie={[]}/>} />
-      <Section title="Movies Crew" content={<Tile movie={[]}/>} />
+      <Section title="Movies Cast" content={<MoviesTile movie={[]}/>} />
+      <Section title="Movies Crew" content={<MoviesTile movie={[]}/>} />
     </Container>
   );
 };
